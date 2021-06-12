a13f49855b821beec21eca693d63393ea72cf18e
Erik Pukinskis <erik@snowedin.net>
2021-06-11 22:23:15 -0700

    Add a shiny plane

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,43 @@ fn main() {
             height: 600.0,
             ..Default::default()
         })
+        .add_startup_system(setup.system())
         .add_plugin(HelloPlugin)
         .add_plugins(DefaultPlugins)
         .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands
+        // Plane
+        .spawn()
+        .insert_bundle(PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Plane { size: 8.0 })),
+            material: materials.add(Color::rgb(1., 0.9, 0.9).into()),
+            transform: Transform::from_translation(Vec3::new(4., 0., 4.)),
+            ..Default::default()
+        });
+    commands
+        // Camera
+        .spawn()
+        .insert_bundle(PerspectiveCameraBundle {
+            transform: Transform::from_matrix(Mat4::from_rotation_translation(
+                Quat::from_xyzw(-0.3, -0.5, -0.3, 0.5).normalize(),
+                Vec3::new(-7.0, 20.0, 4.0),
+            )),
+            ..Default::default()
+        });
+    commands
+        // Light
+        .spawn()
+        .insert_bundle(LightBundle {
+            transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
+            ..Default::default()
+        });
 }
 
 struct Person;
