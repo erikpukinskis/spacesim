0230d523125be46ef8f52835c9217aca0f617e69
Erik Pukinskis <erik@snowedin.net>
2021-06-12 00:15:37 -0700

    a star in space

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,15 @@ use bevy::prelude::*;
 
 fn main() {
     App::build()
+        .add_plugins(DefaultPlugins)
         .insert_resource(WindowDescriptor {
             title: "Untitled Space Sim!".to_string(),
             width: 600.0,
             height: 600.0,
             ..Default::default()
         })
+        .insert_resource(ClearColor(Color::rgb(0.0, 0.01, 0.02)))
         .add_startup_system(setup.system())
-        .add_plugin(HelloPlugin)
-        .add_plugins(DefaultPlugins)
         .run();
 }
 
@@ -25,9 +25,13 @@ fn setup(
         .insert_bundle(PbrBundle {
             mesh: meshes.add(Mesh::from(shape::Icosphere {
                 radius: 4.0,
-                subdivisions: 4,
+                subdivisions: 8,
             })),
-            material: materials.add(Color::rgb(1., 0.9, 0.9).into()),
+            material: materials.add(StandardMaterial {
+                base_color: Color::hex("fff8c1").unwrap(),
+                unlit: true,
+                ..Default::default()
+            }),
             transform: Transform::from_translation(Vec3::new(4., 0., 4.)),
             ..Default::default()
         });
@@ -48,45 +52,4 @@ fn setup(
             transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
             ..Default::default()
         });
-}
-
-struct Person;
-
-struct Name(String);
-
-fn add_people(mut commands: Commands) {
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Elaina Proctor".to_string()));
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Renzo Hume".to_string()));
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Zayna Nieves".to_string()));
-}
-
-struct GreetTimer(Timer);
-
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut AppBuilder) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
-            .add_startup_system(add_people.system())
-            .add_system(greet_people.system());
-    }
-}
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    // update our timer with the time elapsed since the last update
-    // if that caused the timer to finish, we say hello to everyone
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in query.iter() {
-            println!("hello {}!", name.0);
-        }
-    }
 }
